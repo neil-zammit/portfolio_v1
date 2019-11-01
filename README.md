edca46f1c45cb3a8190494c8439e58ed11822ab1
Neil Zammit <nz3001@gmail.com>
2019-11-01 19:54:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>Portfolio V1</h1>
+
+My first developer portfolio
+
+Checklist:
+
+[] My Work Page / Section
+
+[] Contact Page / Section
+
+[] Credits
---
+++ js/index.js
@@ -0,0 +1,36 @@
+// Get html elements
+// menu = document.querySelector('.menu');
+// about = document.querySelector('.about');
+aboutBtn = document.getElementById('about-btn');
+crossBtn = document.querySelector('.cross');
+
+// Functions
+// Change screen function
+// function removeMenu() {
+//   menu.classList.add('hidden');
+// }
+
+// function showAbout() {
+//   about.classList.add('show');
+// }
+
+// // Add EventListener
+// aboutBtn.addEventListener('click', () => {
+//   removeMenu();
+//   showAbout();
+// });
+
+// Add EventListener
+aboutBtn.addEventListener('click', () => {
+  // Push menu to the right and render invisible
+  TweenMax.to('.menu', 1.5, { x: 200, opacity: 0 });
+  // Bring About section to menu's initial position and render visible
+  TweenMax.to('.about', 2.5, { y: -420, opacity: 1 });
+});
+
+crossBtn.addEventListener('click', () => {
+  // Push menu to original position and render visible
+  TweenMax.to('.menu', 2.5, { x: 0, opacity: 1 });
+  // Bring About section to initial position and render invisible
+  TweenMax.to('.about', 1.5, { y: 420, opacity: 0 });
+});
