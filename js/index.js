a7ba89f65fe3877e6668810ea1f5849036f012ed
Neil Zammit <nz3001@gmail.com>
2019-11-02 13:50:42 +0100

    Full width styling complete

--- js/index.js
+++ js/index.js
@@ -2,7 +2,44 @@
 // menu = document.querySelector('.menu');
 // about = document.querySelector('.about');
 aboutBtn = document.getElementById('about-btn');
-crossBtn = document.querySelector('.cross');
+aboutCrossBtn = document.querySelector('.about-cross');
+contactBtn = document.getElementById('contact-btn');
+contactCrossBtn = document.querySelector('.contact-cross');
+
+// Add EventListeners for About Section
+aboutBtn.addEventListener('click', () => {
+  // Push menu to the right and render invisible
+  TweenMax.to('.menu', 1.5, { x: 200, opacity: 0 });
+  // Bring About section to menu's initial position and render visible
+  TweenMax.to('.about', 2.5, { y: -420, opacity: 1 });
+});
+
+aboutCrossBtn.addEventListener('click', () => {
+  // Push menu to original position and render visible
+  TweenMax.to('.menu', 2.5, { x: 0, opacity: 1 });
+  // Bring About section to initial position and render invisible
+  TweenMax.to('.about', 1.5, { y: 420, opacity: 0 });
+});
+
+// Add EventListeners for Contact Section
+contactBtn.addEventListener('click', () => {
+  // Push menu to the right and render invisible
+  TweenMax.to('.menu', 1.5, { x: 200, opacity: 0 });
+  // Bring Contact section to menu's initial position and render visible
+  TweenMax.to('.contact', 2, { y: -803, opacity: 1 });
+});
+
+contactCrossBtn.addEventListener('click', () => {
+  // Push menu to original position and render visible
+  TweenMax.to('.menu', 2.4, { x: 0, opacity: 1 });
+  // Bring Contact section to initial position and render invisible
+  TweenMax.to('.contact', 2.3, { y: 803, opacity: 0 });
+});
+
+// Init Smooth Scroll for all anchor tags
+const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 3000
+});
 
 // Functions
 // Change screen function
@@ -19,23 +56,3 @@ crossBtn = document.querySelector('.cross');
 //   removeMenu();
 //   showAbout();
 // });
-
-// Add EventListener
-aboutBtn.addEventListener('click', () => {
-  // Push menu to the right and render invisible
-  TweenMax.to('.menu', 1.5, { x: 200, opacity: 0 });
-  // Bring About section to menu's initial position and render visible
-  TweenMax.to('.about', 2.5, { y: -420, opacity: 1 });
-});
-
-crossBtn.addEventListener('click', () => {
-  // Push menu to original position and render visible
-  TweenMax.to('.menu', 2.5, { x: 0, opacity: 1 });
-  // Bring About section to initial position and render invisible
-  TweenMax.to('.about', 1.5, { y: 420, opacity: 0 });
-});
-
-// Init Smooth Scroll for all anchor tags
-const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 3000
-});
